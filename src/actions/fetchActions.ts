63d2e2f9381655f0e44809a1b2ef7a24d2fc61ef
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-10-09 03:54:27 +0530

    chore: update fetch actions and impliment unstable cache for optimaztion and avoid prisma query logs for each reload.

--- src/actions/fetchActions.ts
+++ src/actions/fetchActions.ts
@@ -1,14 +1,17 @@
 import { prisma } from "@/lib/db.config";
+import { unstable_cache } from "next/cache";
 
-export const getUserCoins = async (user_id: number | string) => {
-  const coins = await prisma.user.findUnique({
-    select: {
-      coins: true,
-    },
-    where: {
-      id: Number(user_id),
-    },
-  });
-
-  return coins;
-};
+export const getUserCoins = unstable_cache(
+  async (user_id: number | string) => {
+    return await prisma.user.findUnique({
+      select: {
+        coins: true,
+      },
+      where: {
+        id: Number(user_id),
+      },
+    });
+  },
+  ["userCoins"],
+  { revalidate: 60 * 60, tags: ["userCoins"] }
+);
