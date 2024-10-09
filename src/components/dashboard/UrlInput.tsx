5251c1584530f0fb769d84eb995b3f38615e1cce
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-10-10 05:18:25 +0530

    chore: Update URL Input Component

---
+++ src/components/dashboard/UrlInput.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { useActionState, useState } from "react";
+import Loading from "../common/Loading";
+
+export default function UrlInput() {
+  const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = (event: React.FormEvent) => {};
+
+  return (
+    <div className="flex justify-center items-center mt-10 w-full">
+      <form onSubmit={handleSubmit} className="relative w-full md:w-[500px]">
+        <input
+          className="w-full md:w-[500px] h-12 rounded-md bg-muted border border-slate-400 border-dashed p-3 outline-none"
+          type="url"
+          placeholder="Enter URL Of The Video You Want To Summarize..."
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          disabled={loading}
+        />
+        <div className="">
+          <Loading />
+        </div>
+      </form>
+    </div>
+  );
+}
