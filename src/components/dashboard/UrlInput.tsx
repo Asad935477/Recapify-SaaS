28df51028cf5795952846e6db42585f616556977
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-10-09 22:56:47 +0530

    feat: Add URL state and onChange handler to UrlInput component

--- src/components/dashboard/UrlInput.tsx
+++ src/components/dashboard/UrlInput.tsx
@@ -1,8 +1,11 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 export default function UrlInput() {
+  const [url, setUrl] = useState("");
+
   const handleSubmit = (event: React.FormEvent) => {};
+
   return (
     <div className="flex justify-center items-center mt-10 w-full">
       <form onSubmit={handleSubmit} className="relative w-full md:w-[500px]">
@@ -10,6 +13,8 @@ export default function UrlInput() {
           className="w-full md:w-[500px] h-12 rounded-md bg-muted border border-slate-400 border-dashed p-3 outline-none"
           type="url"
           placeholder="Enter URL Of The Video You Want To Summarize..."
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
         />
       </form>
     </div>
