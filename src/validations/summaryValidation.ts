fd85051679e5de933200cd7734bd35171954a884
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-10-12 23:43:40 +0530

    WIP: Work In Progress for Summary Validation

---
+++ src/validations/summaryValidation.ts
@@ -0,0 +1,10 @@
+import vine from '@vinejs/vine'
+import { CustomErrorReporter } from './CustomErrorReporter';
+
+vine.errorReporter = () => new CustomErrorReporter()
+
+export const summarySchema = vine.object({
+    url:vine.string().url(),
+    user_id: vine.string(),
+
+});
