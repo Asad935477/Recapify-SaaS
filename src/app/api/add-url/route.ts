451a9c4312205184a7ccc089745e62fbbd5bc84b
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-10-13 23:17:04 +0530

    feat: add response payload sending functionality and improved error handling

--- src/app/api/add-url/route.ts
+++ src/app/api/add-url/route.ts
@@ -1,16 +1,32 @@
+import { summarySchema } from "@/validations/summaryValidation";
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
+import vine, { errors } from "@vinejs/vine";
 
-
-export async function POST(req:NextRequest) {
-    try {
-        const token = await getToken({req})
-        if(!token){
-            return NextResponse.json({message:"UnAuthorized"}, {status:401});
-        }
-
-    } catch (error) {
-        console.log("The Add URL Error",error)
-        return NextResponse.json({message:"Something Went Wrong!!! Please \n Try Again Later..."})
+export async function POST(req: NextRequest) {
+  try {
+    const token = await getToken({ req });
+    if (!token) {
+      return NextResponse.json({ message: "UnAuthorized" }, { status: 401 });
     }
+
+    const body = await req.json();
+    const Validator = vine.compile(summarySchema);
+    const payload = await Validator.validate(body);
+    return NextResponse.json({ payload });
+  } catch (error) {
+    console.log("The Add URL Error", error);
+    if (error instanceof errors.E_VALIDATION_ERROR) {
+      return NextResponse.json(
+        {
+          message: "Please Check Validation Errors",
+          errors: error.messages,
+        },
+        { status: 422 }
+      );
+    }
+    return NextResponse.json({
+      message: `Something Went Wrong!!! Please Try Again Later...`,
+    });
+  }
 }
