44610edf512aec947ec3204e31a5f355dd3df036
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-10-13 23:19:05 +0530

    feat: add summary Validation functionality and implement Custom Error Reporter for POST Requests

--- src/validations/summaryValidation.ts
+++ src/validations/summaryValidation.ts
@@ -1,10 +1,9 @@
-import vine from '@vinejs/vine'
-import { CustomErrorReporter } from './CustomErrorReporter';
+import vine from "@vinejs/vine";
+import { CustomErrorReporter } from "./CustomErrorReporter";
 
-vine.errorReporter = () => new CustomErrorReporter()
+vine.errorReporter = () => new CustomErrorReporter();
 
 export const summarySchema = vine.object({
-    url:vine.string().url(),
-    user_id: vine.string(),
-
+  url: vine.string().url(),
+  user_id: vine.string(),
 });
