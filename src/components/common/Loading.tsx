aa1e444181391d3d37c96336812b7c892f676e2c
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-10-17 22:03:48 +0530

    chore: Change the loading wheel icon color to match with the theme of the application

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="animate-spin w-6 h-6 border-t-2 border-b-2  border-pink-500 rounded-full"></div>
+    <div className="animate-spin w-6 h-6 border-t-2 border-b-2  border-slate-500 rounded-full"></div>
   );
 }
