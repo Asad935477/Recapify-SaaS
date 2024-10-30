01197f95f3bc3ae998308bc6186902085672cd94
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-10-30 23:25:45 +0530

    Feat: Add SummaryType definition to types.ts

--- src/Types.ts
+++ src/Types.ts
@@ -6,3 +6,12 @@ type addUrlErrorType = {
   url?: string;
   user_id?: string;
 };
+
+type SummaryType = {
+  url: string;
+  user_id: number;
+  title: string;
+  id: string;
+  created_at: Date;
+  response: string | null;
+};
