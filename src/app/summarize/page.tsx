62fcea80a2bb18b373eed524787337bbf7ab14dc
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-09 23:36:19 +0530

    WIP: Work In Progress for Summarize Page

--- src/app/summarize/page.tsx
+++ src/app/summarize/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Summarize() {
-  return <div>Summarize</div>;
+function Summarize({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  if (!searchParams) return <div>Summarize</div>;
 }
 
 export default page;
