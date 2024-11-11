aa730ae07795bf89fddc0ce1110e6f68db538ccd
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-11 22:59:05 +0530

    Fix: correct the type for the URL Input

--- src/components/dashboard/UrlInput.tsx
+++ src/components/dashboard/UrlInput.tsx
@@ -42,7 +42,7 @@ export default function UrlInput({ user }: { user: CustomUser }) {
       <form onSubmit={handleSubmit} className="relative w-full md:w-[500px]">
         <input
           className="w-full md:w-[500px] h-12 rounded-md bg-muted border border-slate-400 border-dashed p-3 outline-none"
-          type="text"
+          type="url"
           placeholder="Enter URL Of The Video You Want To Summarize..."
           value={url}
           onChange={(e) => setUrl(e.target.value)}
