bcc2d19ec3e1530e68508278052bf527a5d68b51
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-14 23:28:35 +0530

    Feat: Initialize endpoint for summarizing payload

---
+++ src/app/api/summarize/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface SummerizePayload {
+  url: string;
+  id: string;
+}
+
+export async function POST(request: NextRequest) {
+  try {
+  } catch (error) {}
+}
