83a624cb33c296e640d2ab16d6de099124dbc990
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-14 23:35:08 +0530

    WIP: Add endpoint setup for summarizing payload and work in progress for summarization logic

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+import { authOptions, CustomSession } from "../auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 
 interface SummerizePayload {
   url: string;
@@ -7,6 +9,7 @@ interface SummerizePayload {
 
 export async function POST(request: NextRequest) {
   try {
+    const session: CustomSession | null = await getServerSession(authOptions);
   } catch (error) {
     return NextResponse.json(
       { message: `Something Went Wrong!!! Please Try Again Later...` },
