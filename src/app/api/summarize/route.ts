e206cd0d1eb8e002ca426069b137b092f017173f
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-15 22:49:13 +0530

    Feat: Add POST endpoint for summarizing payload with token validation

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions, CustomSession } from "../auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
+import { getUserCoins } from "@/actions/fetchActions";
 
 interface SummerizePayload {
   url: string;
@@ -12,6 +13,19 @@ export async function POST(request: NextRequest) {
     const session: CustomSession | null = await getServerSession(authOptions);
     if (!session) {
       return NextResponse.json({ message: "UnAuthorized" }, { status: 401 });
+    }
+    const body: SummerizePayload = await request.json();
+
+    //*CHECK IF USER HAS SUFFICIENT COINS TO MAKE REQUESTS OR NOT
+    const userCoins = await getUserCoins(session?.user?.id!);
+    if (userCoins === null || (userCoins?.coins && userCoins?.coins <= 10)) {
+      return NextResponse.json(
+        {
+          message:
+            "You Don't Have Sufficient Coins To Make More Requests. Please Add More Coins To Continue Using Our Services",
+        },
+        { status: 400 }
+      );
     }
   } catch (error) {
     return NextResponse.json(
