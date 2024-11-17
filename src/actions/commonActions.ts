d1cdea876f7d1d1656122fa92818589b87cc0ab5
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-17 22:13:54 +0530

    feat: Add minusCoins function to decrement user coins for each request

---
+++ src/actions/commonActions.ts
@@ -0,0 +1,12 @@
+export async function minusCoins(user_id: number | string): Promise<void> {
+  await prisma.user.update({
+    where: {
+      id: Number(user_id),
+    },
+    data: {
+      coins: {
+        decrement: 10,
+      },
+    },
+  });
+}
