71ecc8999c92bdfcb825977c755a776780d05156
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-21 02:13:11 +0530

    feat: Add response state and useEffect to SummaryBase component

--- src/components/summary/SummaryBase.tsx
+++ src/components/summary/SummaryBase.tsx
@@ -1,14 +1,27 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SummarizeLoader from "./SummarizeLoader";
 
 export default function SummaryBase({ summary }: { summary: ChatType | null }) {
   const [loading, setLoading] = useState(true);
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    if (summary?.response) {
+      setResponse(summary?.response!);
+      setLoading(false);
+    }
+  }, [summary]);
 
   return (
     <div className="flex items-center flex-col w-full">
       <h1 className="text-2xl font-bold my-4">{summary?.title}</h1>
       {loading && <SummarizeLoader />}
+      {response && (
+        <div className="w-full md:w-[700px] rounded-lg bg-slate-100 shadow-md p-8">
+          {response}
+        </div>
+      )}
     </div>
   );
 }
