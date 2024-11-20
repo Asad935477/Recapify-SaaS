433fb3d11c864139c4998172e99a833b663a088d
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-21 02:11:58 +0530

    feat: Add loading state to SummaryBase component

--- src/components/summary/SummaryBase.tsx
+++ src/components/summary/SummaryBase.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import SummarizeLoader from "./SummarizeLoader";
 
 export default function SummaryBase({ summary }: { summary: ChatType | null }) {
   const [loading, setLoading] = useState(true);
@@ -8,7 +7,7 @@ export default function SummaryBase({ summary }: { summary: ChatType | null }) {
   return (
     <div className="flex items-center flex-col w-full">
       <h1 className="text-2xl font-bold my-4">{summary?.title}</h1>
-      {loading && <SummarizeLoader />}
+      {loading && <div>Loading...</div>}
     </div>
   );
 }
