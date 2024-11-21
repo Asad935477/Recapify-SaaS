7732a71d8024db2b6b5af4db7ac5ef0bb14c1216
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-22 01:16:22 +0530

    feat: Initialize OldSummaryCard component with dependencies

---
+++ src/components/dashboard/OldSummaryCard.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+export default function OldSummaryCard({
+  summary,
+}: {
+  summary: UserSummaries;
+}) {
+  return <div></div>;
+}
