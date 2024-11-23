39a7a1f61f628d3ff947d65f1d0f21e28b95a92e
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-23 23:28:02 +0530

    feat: Add basic structure to CoinsSpend component

--- src/app/coins-spend/page.tsx
+++ src/app/coins-spend/page.tsx
@@ -13,5 +13,12 @@ export default async function CoinsSpend() {
   const session: CustomSession | null = await getServerSession(authOptions);
   const userCoins = await getUserCoins(session?.user?.id!);
   const coinsSpends = await getCoinsSpend(session?.user?.id!);
-  return <div></div>;
+  return (
+    <div className="container">
+      <DashNav user={session?.user!} userCoins={userCoins} />
+      <div className="text-center w-full">
+        <h1 className="text-2xl font-bold mb-4">Coins Spend History</h1>
+      </div>
+    </div>
+  );
 }
