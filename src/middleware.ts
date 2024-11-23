e2295d70fc19dee67988b98c99eee31653ed531f
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-23 23:32:53 +0530

    feat: Add /transactions route to Next.js middleware configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboard", "/summarize", "/coins-spend"] };
+export const config = {
+  matcher: ["/dashboard", "/summarize", "/coins-spend", "/transactions"],
+};
