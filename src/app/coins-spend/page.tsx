5a00b4e5559638191b25339f09a9e6b37558adfe
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-23 23:30:53 +0530

    refactor: Remove unused import from CoinsSpend component

--- src/app/coins-spend/page.tsx
+++ src/app/coins-spend/page.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { authOptions, CustomSession } from "../api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
-import {
-  getCoinsSpend,
-  getTransactions,
-  getUserCoins,
-} from "@/actions/fetchActions";
+import { getCoinsSpend, getUserCoins } from "@/actions/fetchActions";
 import DashNav from "@/components/dashboard/DashNav";
 import Link from "next/link";
 
