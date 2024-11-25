c96381c27dacd9748368323a41d68978c71d8839
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-25 22:52:35 +0530

    feat: Define SessionPayload interface

--- src/app/api/stripe/sesssion/route.ts
+++ src/app/api/stripe/sesssion/route.ts
@@ -1,3 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions, CustomSession } from "../../auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
+
+interface SessionPayload {
+  plan: String;
+}
