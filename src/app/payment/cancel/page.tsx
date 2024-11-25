f268ca57f328be919a54a7018d90367495b8a33d
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-26 03:13:21 +0530

    feat: Define SuccessTxn component structure

--- src/app/payment/cancel/page.tsx
+++ src/app/payment/cancel/page.tsx
@@ -2,3 +2,9 @@ import { clearCache } from "@/actions/commonActions";
 import notFound from "@/app/not-found";
 import prisma from "@/lib/db.config";
 import Image from "next/image";
+
+async function SuccessTxn({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {}
