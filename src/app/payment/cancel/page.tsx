fcf4b437697801103cac54c07c705fb482df506c
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-26 03:21:50 +0530

    feat: Add JSX return for SuccessTxn component with Image

--- src/app/payment/cancel/page.tsx
+++ src/app/payment/cancel/page.tsx
@@ -9,22 +9,22 @@ async function SuccessTxn({
   searchParams: { [key: string]: string | undefined };
 }) {
   const transaction = await prisma.transactions.findUnique({
-    where: {
-      id: searchParams?.["txnId"],
-      status: 2,
-    },
+    where: { id: searchParams?.["txnId"], status: 2 },
   });
   if (!transaction) {
     return notFound();
   }
   await prisma.transactions.update({
-    data: {
-      status: 0,
-    },
-    where: {
-      id: searchParams?.["txnId"],
-    },
+    data: { status: 0 },
+    where: { id: searchParams?.["txnId"] },
   });
-
   clearCache("transactions");
+  return (
+    <div className="h-screen flex justify-center items-center flex-col">
+      <Image src="/images/cancel.png" width={512} height={512} alt="success" />
+      <h1 className="text-3xl font-bold text-green-400">
+        Payment Cancelled By The User!
+      </h1>
+    </div>
+  );
 }
