6984f97d36738fd57f7e578fd09e3a446b5cf945
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-26 03:37:34 +0530

    feat: Refactor Stripe session creation and error

--- src/components/landing/Pricing.tsx
+++ src/components/landing/Pricing.tsx
@@ -6,30 +6,31 @@ import { Button } from "../ui/button";
 import { CustomUser } from "@/app/api/auth/[...nextauth]/options";
 import { toast } from "sonner";
 import axios, { AxiosError } from "axios";
+import getStripe from "@/lib/stripe";
 // import getStripe from "@/lib/stripe";
 export default function Pricing({ user }: { user?: CustomUser }) {
   const [loading, setLoading] = useState(false);
 
   const initiatePayment = async (plan: string) => {
-    // if (!user) {
-    //   toast.error("Please login first.");
-    // }
-    // setLoading(true);
-    // try {
-    //   const { data } = await axios.post("/api/stripe/session", { plan: plan });
-    //   if (data?.id) {
-    //     const stripe = await getStripe();
-    //     await stripe?.redirectToCheckout({ sessionId: data?.id });
-    //   }
-    //   setLoading(false);
-    // } catch (error) {
-    //   setLoading(false);
-    //   if (error instanceof AxiosError) {
-    //     toast.error(error?.response?.data?.message);
-    //   } else {
-    //     toast.error("Something went wrong.please try again!");
-    //   }
-    // }
+    if (!user) {
+      toast.error("Please login first.");
+    }
+    setLoading(true);
+    try {
+      const { data } = await axios.post("/api/stripe/session", { plan: plan });
+      if (data?.id) {
+        const stripe = await getStripe();
+        await stripe?.redirectToCheckout({ sessionId: data?.id });
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      if (error instanceof AxiosError) {
+        toast.error(error?.response?.data?.message);
+      } else {
+        toast.error("Something went wrong.please try again!");
+      }
+    }
   };
 
   return (
