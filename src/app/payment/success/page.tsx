4577e2015c536489a9f76bc38ac530f0aea27716
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-26 03:33:48 +0530

    feat: Add JSX return for SuccessTxn component with Image

--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -31,4 +31,13 @@ async function SuccessTxn({
   await addCoins(transaction.user_id, getCoinsFromAmount(transaction.amount));
   clearCache("userCoins");
   clearCache("transactions");
+
+  return (
+    <div className="h-screen flex justify-center items-center flex-col">
+      <Image src="/images/check.png" width={512} height={512} alt="success" />
+      <h1 className="text-3xl font-bold text-green-400">
+        Payment Processed Successfully
+      </h1>
+    </div>
+  );
 }
