85296cc3e792c1f487ff34a2cab973d552fa1992
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-26 03:26:40 +0530

    fix: Add missing Export and correct the name of the component to CancelTxn

--- src/app/payment/cancel/page.tsx
+++ src/app/payment/cancel/page.tsx
@@ -3,7 +3,7 @@ import notFound from "@/app/not-found";
 import prisma from "@/lib/db.config";
 import Image from "next/image";
 
-async function SuccessTxn({
+async function CancelTxn({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
@@ -21,10 +21,18 @@ async function SuccessTxn({
   clearCache("transactions");
   return (
     <div className="h-screen flex justify-center items-center flex-col">
-      <Image src="/images/cancel.png" width={512} height={512} alt="success" />
+      {" "}
+      <Image
+        src="/images/cancel.png"
+        width={512}
+        height={512}
+        alt="success"
+      />{" "}
       <h1 className="text-3xl font-bold text-green-400">
-        Payment Cancelled By The User!
-      </h1>
+        {" "}
+        Payment Cancelled By The User!{" "}
+      </h1>{" "}
     </div>
   );
 }
+export default CancelTxn;
