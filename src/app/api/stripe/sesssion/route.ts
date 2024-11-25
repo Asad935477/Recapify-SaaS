22e02ec817d3e5ef58e2e3e765f78cff937cb0cf
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-11-26 04:13:58 +0530

    fix: Add missing Imports

--- src/app/api/stripe/sesssion/route.ts
+++ src/app/api/stripe/sesssion/route.ts
@@ -1,23 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions, CustomSession } from "../../auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
-import Stripe from "stripe";
 import prisma from "@/lib/db.config";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+import Stripe from "stripe";
 
 interface SessionPayload {
-  plan: String;
+  plan: string;
 }
 
 export async function POST(req: NextRequest) {
+  const session: CustomSession | null = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ message: "UnAuthorized" }, { status: 401 });
+  }
   try {
-    const session: CustomSession | null = await getServerSession(authOptions);
-    if (!session) {
-      return NextResponse.json({ message: "UnAuthorized" }, { status: 401 });
-    }
     const body: SessionPayload = await req.json();
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-    //* GET PRODUCT
+    // * Get Product
     const product = await prisma.products.findUnique({
       where: {
         name: body.plan,
@@ -26,14 +26,12 @@ export async function POST(req: NextRequest) {
 
     if (!product) {
       return NextResponse.json(
-        {
-          message: "No Product found with this plan.",
-        },
+        { message: "No product found.please check you passed correct product" },
         { status: 404 }
       );
     }
 
-    // CREATE TRANSACTION
+    // * Create Transaction
     const transaction = await prisma.transactions.create({
       data: {
         user_id: Number(session?.user?.id!),
@@ -41,14 +39,14 @@ export async function POST(req: NextRequest) {
       },
     });
 
-    //* CREATE STRIPE SESSION
+    // * Create Stripe Session
     const stripeSession = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       currency: "INR",
       billing_address_collection: "required",
       line_items: [
         {
-          price: product.price_id,
+          price: product?.price_id,
           quantity: 1,
         },
       ],
@@ -58,12 +56,13 @@ export async function POST(req: NextRequest) {
     });
 
     return NextResponse.json({
-      message: "Session Generated Successfully",
+      message: "Session generated successfully!",
       id: stripeSession?.id,
     });
   } catch (error) {
+    console.log("The error is", error);
     return NextResponse.json(
-      { message: `Something Went Wrong!!! Please Try Again Later...` },
+      { message: "Something went wrong.please try again!" },
       { status: 500 }
     );
   }
