616b645dffe47d486165c53480ea9c927e714655
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2024-12-21 11:51:34 +0530

    fix : correct stripe payments bug

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,13 +1,9 @@
 import { Stripe, loadStripe } from "@stripe/stripe-js";
-import exp from "constants";
 
-let stripePromise: promise<Stripe | null>;
-
+let stripePromise: Promise<Stripe | null>;
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(
-      process.env.NEXTAUTH_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-    );
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
   }
   return stripePromise;
 };
